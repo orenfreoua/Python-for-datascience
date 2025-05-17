f3a6d67127ad7d25ced3f1a7e9fc4e03150c4d38
orenfreoua <orenfreoua42@gmail.com>
2025-05-17 13:04:26 +0200

    feat: implement Morse code encoder with argument validation and error handling

---
+++ python_0/ex07/sos.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""Morse Code Encoder"""
+
+import sys
+
+NESTED_MORSE = {
+    "A": ".- ", "B": "-... ", "C": "-.-. ", "D": "-.. ",
+    "E": ". ", "F": "..-. ", "G": "--. ", "H": ".... ",
+    "I": ".. ", "J": ".--- ", "K": "-.- ", "L": ".-.. ",
+    "M": "-- ", "N": "-. ", "O": "--- ", "P": ".--. ",
+    "Q": "--.- ", "R": ".-. ", "S": "... ", "T": "- ",
+    "U": "..- ", "V": "...- ", "W": ".-- ", "X": "-..- ",
+    "Y": "-.-- ", "Z": "--.. ",
+    "0": "----- ", "1": ".---- ", "2": "..--- ", "3": "...-- ",
+    "4": "....- ", "5": "..... ", "6": "-.... ", "7": "--... ",
+    "8": "---.. ", "9": "----. ",
+    " ": "/ "
+}
+
+
+class Colors:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    END = '\033[0m'
+
+
+def main() -> None:
+
+    try:
+        assert len(sys.argv) == 2, "Exactly one argument is required"
+        arg = sys.argv[1]
+        assert all(char.isalnum() or char.isspace() for char in arg), \
+            "the arguments are bad"
+        morse_text = ""
+        for char in arg.upper():
+            if char not in NESTED_MORSE:
+                raise AssertionError(
+                    "the argument must contain only alphanumeric char or "
+                    "spaces."
+                )
+            morse_text += NESTED_MORSE[char]
+        print(morse_text.strip())
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+
+
+if __name__ == "__main__":
+    main()
