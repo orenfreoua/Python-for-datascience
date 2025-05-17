abac71f796873237ec6483722873b37899417edb
orenfreoua <orenfreoua42@gmail.com>
2025-05-17 16:26:49 +0200

    adjustement of the ex00

--- python_0/ex00/Hello.py
+++ python_0/ex00/Hello.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
-ft_list = ["Hello", "World!"]
-ft_tuple = ("Hello", "France!")
-ft_set = {"Paris!", "Hello"}
-ft_dict = {"Hello" : "42Paris!"}
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
+
+ft_list[1] = "World!"
+ft_tuple = (ft_tuple[0], "France!")
+ft_set = {"Hello", "Paris!"}
+ft_dict["Hello"] = "42Paris!"
 
 print(ft_list)
 print(ft_tuple)
